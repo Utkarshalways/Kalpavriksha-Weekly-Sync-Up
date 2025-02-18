b4aac88168744ab74131bd61c4ec13e4b41baee2
utkarshalways <utkarshshrma08@gmail.com>
2025-02-18 16:27:41 +0530

    Implemented Hashmap in Valid Parentheses Question

--- validParentheses.c
+++ validParentheses.c
@@ -4,36 +4,30 @@
 #include <stdlib.h>
 #define SIZE_TABLE 256
 
-typedef struct Node{
+typedef struct Node {
     char ch;
     int count;
     struct Node* next;
+} Node;
 
-}Node;
-
-
-
-typedef struct Hashmap{
+typedef struct Hashmap {
     struct Node* table[SIZE_TABLE];
-}Hashmap;
+} Hashmap;
 
-Node* createNode(char ch){
-
+Node* createNode(char ch) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->ch = ch;
     newNode->count = 0;
     newNode->next = NULL;
-
+    return newNode;
 }
-Hashmap* createMap(int size){
 
+Hashmap* createMap() {
     Hashmap* map = (Hashmap*)malloc(sizeof(Hashmap));
-    for(int idx = 0;idx < size;idx++){
+    for (int idx = 0; idx < SIZE_TABLE; idx++) {
         map->table[idx] = NULL;
     }
-
     return map;
-
 }
 
 int hashFunction(char ch) {
@@ -70,88 +64,90 @@ int getCount(Hashmap* map, char ch) {
     return 0;
 }
 
+void printMap(Hashmap* map) {
+    for (int i = 0; i < SIZE_TABLE; i++) {
+        Node* curr = map->table[i];
+        if (curr != NULL) {
+            printf("Index %d: ", i);
+            while (curr != NULL) {
+                printf("(%c, %d) -> ", curr->ch, curr->count);
+                curr = curr->next;
+            }
+            printf("NULL\n");
+        }
+    }
+}
 
-Node* push(Node* head, char ch){
-
+Node* push(Node* head, char ch) {
     Node* newNode = createNode(ch);
-    if(head == NULL){
+    if (head == NULL) {
         return newNode;
     }
 
     newNode->next = head;
     head = newNode;
     return head;
-
 }
 
-char peek(Node* head){
+char peek(Node* head) {
     return head->ch;
 }
 
-char pop(Node** head){
-
-    if((*head) == NULL){
+char pop(Node** head) {
+    if ((*head) == NULL) {
         return '0';
     }
     char temp = (*head)->ch;
+    Node* toDelete = *head;
     (*head) = (*head)->next;
+    free(toDelete);
     return temp;
-
 }
 
-
-bool isValid(char* str){
+bool isValid(Hashmap* map, char* str) {
     Node* head = NULL;
-    for(int i = 0;i<strlen(str);i++){
-
-        
-        if(str[i] == '{' || str[i] == '[' || str[i] == '('){
-            head = push(head,str[i]);
-        }
-        else{
-
-            if(str[i] == '}' ){
-                
-                if(peek(head) != '{'){
+    
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == '{' || str[i] == '[' || str[i] == '(') {
+            head = push(head, str[i]);
+            insert(map, str[i]);
+        } else {
+            if (str[i] == '}') {
+                if (peek(head) != '{') {
                     return false;
-                }else{
+                } else {
+                    insert(map, str[i]);
                     pop(&head);
                 }
-                
-            }
-            else if(str[i] == ']'){
-                 
-                if(peek(head) != '['){
+            } else if (str[i] == ']') {
+                if (peek(head) != '[') {
                     return false;
-                }else{
+                } else {
+                    insert(map, str[i]);
                     pop(&head);
                 }
-                
-            }else if(str[i] == ')'){
-                 
-                if(peek(head) != '('){
+            } else if (str[i] == ')') {
+                if (peek(head) != '(') {
                     return false;
-                }else{
+                } else {
+                    insert(map, str[i]);
                     pop(&head);
                 }
-                
             }
         }
-
     }
-
-    if(head != NULL){
+    
+    if (head != NULL) {
         return false;
     }
     return true;
-    
 }
 
-
-int main(){
-
+int main() {
     char str[100];
-    scanf("%s",str);
-    isValid(str)?printf("True\n"):printf("False");
-
+    scanf("%s", str);
+    Hashmap* map = createMap();
+    isValid(map, str) ? printf("True\n") : printf("False");
+    printMap(map);
+    return 0;
 }
